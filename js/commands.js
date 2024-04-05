27629114b9dfdc9b80252b3fe3eb5449091eba3e
deshanlankal <deshanlanka500@gmail.com>
2024-04-05 13:48:33 +0530

    first commit

---
+++ js/commands.js
@@ -0,0 +1,98 @@
+
+var twitter = "https://twitter.com/deshanlanka_l/";
+var password = "deshan";
+var linkedin = "https://www.linkedin.com/in/deshanlankal/";
+var instagram = "https://instagram.com/deshanlanka.l/";
+var github = "https://github.com/deshanlankal/";
+var email = 'mailto:deshanlanka500@gmail.com';
+
+whois = [
+    "<br>",
+    "I'm Deshan Lanka👋<br> a dedicated software engineer specializing in full-stack development.",
+    "With a solid foundation in computer science, I've embarked on a journey to become proficient in",
+    "every aspect of software development. From crafting intuitive user interfaces to designing robust backend systems,",
+    "I thrive on bringing ideas to life through code.After earning my Diploma's degree in Software Engineering,",
+    "I delved into the world of professional software engineering. Working on diverse projects, I've gained valuable experience in",
+    "building scalable web applications and solving complex technical challenges.Throughout my career, I've been passionate about",
+    "sharing knowledge and fostering growth within the developer community. Whether it's through mentorship,",
+    "contributing to open-source projects, or participating in tech meetups, I'm always eager to collaborate and learn from others.",
+    "As I continue to evolve in my journey as a software engineer, I'm constantly seeking new opportunities to expand my skills and",
+    "make meaningful contributions to the world of technology. Feel free to reach out if you have any questions or if there's ",
+    "anything I can assist you with!",
+    "<br>"
+  ];
+  
+
+whoami = [
+  "<br>",
+  "The enigma of 'Who am I?' persists: we seldom grasp, yet perpetually explore.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter.com/deshanlanka_l' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin.com/in/deshanlankal/' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/deshanlankal' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github.com/deshanlankal' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Deshan?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+    '<span class="index">Deshan Lanka (DL) Not A Corporation. All Right reserved.</span>',
+    "                                                                                  ,--,                                                 ",
+    "                                                                               ,---.'|                                                 ",
+    "      ,---,                           ,---,                                    |   | :                                 ,-.             ",
+    "    .'  .' `\\                       ,--.' |                                    :   : |                             ,--/ /|             ",
+    "  ,---.'     \\                      |  |  :                      ,---,         |   ' :                     ,---, ,--. :/ |             ",
+    "  |   |  .`\\  |            .--.--.  :  :  :                  ,-+-. /  |        ;   ; '                 ,-+-. /  |:  : ' /              ",
+    "  :   : |  '  |   ,---.   /  /    ' :  |  |,--.  ,--.--.    ,--.'|'   |        '   | |__   ,--.--.    ,--.'|'   ||  '  /    ,--.--.    ",
+    "  |   ' '  ;  :  /     \\ |  :  /`./ |  :  '   | /       \\  |   |  ,\"' |        |   | :.'| /       \\  |   |  ,\"' |'  : |. \\ /       \\   ",
+    "  '   | ;  .  | /    /  ||  :  ;_   |  |   /' :.--.  .-. | |   | /  | |        '   :    ;.--.  .-. | |   | /  | ||  |   \\.--.  .-. |  ",
+    "  |   | :  |  '.    ' / | \\  \\    `.'  :  | | | \\__\\/ : . . |   | |  | |        |   |  ./  \\__\\/ : . . |   | |  | |'  : |. \\ \\__\\/ : . .  ",
+    "  '   : | /  ; '   ;   /|  `----.   \\  |  ' | : ,\" .--.; | |   | |  |/         ;   : ;    ,\" .--.; | |   | |  |/ |  | ' \\ ,\" .--.; |  ",
+    "  |   | '` ,/  '   |  / | /  /`--'  /  :  :_:,'/  /  ,.  | |   | |--'          |   ,/    /  /  ,.  | |   | |--'  '  : |--'/  /  ,.  |  ",
+    "  ;   :  .'    |   :    |'--'.     /|  | ,'   ;  :   .'   \\|   |/              '---'    ;  :   .'   \\|   |/      ;  |,'  ;  :   .'   \\ ",
+    "  |   ,.'       \\   \\  /   `--'---' `--''     |  ,     .-./'---'                        |  ,     .-./'---'       '--'    |  ,     .-./ ",
+    "  '---'          `----'                        `--`---'                                  `--`---'                         `--`---'     ",
+    "             \\  _.-'~~~~'-._          /",
+    "      .      .-~ \\__/  \\__/ ~-.      /   .",
+    "           .-~   (oo)  (oo)    ~-.  /",
+    "          (_____//~~\\\\//~~\\\\______)/",
+    "     _.-~`                         `~-._",
+    "    /O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O=O\\     *",
+    "    \\___________________________________/",
+    "               \\x x x x x x x/",
+    "       .  *     \\x_x_x_x_x_x/",
+    "------------------------------------------------",
+    "'Welcome to my interactive web terminal.'",
+    "--------------------------------------------------------------------------------",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  ];
